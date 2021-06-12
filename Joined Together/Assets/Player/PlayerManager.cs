347823aeda9382edbe707a0767bbb33d3c34ea84
Joshua Tan <jaykid1jaykid1@gmail.com>
2021-06-12 15:28:32 +0930

    added health

--- Joined Together/Assets/Player/PlayerManager.cs
+++ Joined Together/Assets/Player/PlayerManager.cs
@@ -7,7 +7,7 @@ public class PlayerManager : MonoBehaviour
     public static PlayerManager instance;
     public GameObject player1;
     public GameObject player2;
-    public GameObject currentPlayer;
+    [HideInInspector] public GameObject currentPlayer;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +15,8 @@ public class PlayerManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            currentPlayer = player1;
+            currentPlayer = player2;
+            SwitchPlayer();
         }
     }
 
@@ -24,19 +25,25 @@ public class PlayerManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.E))
         {
-            currentPlayer.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            currentPlayer.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation; 
-
-            if (currentPlayer == player1)
-            {
-                currentPlayer = player2;
-            }
-            else
-            {
-                currentPlayer = player1;
-            }
-
-            currentPlayer.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+            SwitchPlayer();
         }
+    }
+
+    void SwitchPlayer()
+    {
+        currentPlayer.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        currentPlayer.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+
+        if (currentPlayer == player1)
+        {
+            currentPlayer = player2;
+        }
+        else
+        {
+            currentPlayer = player1;
+        }
+
+        currentPlayer.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        HealthBar.instance.currentPlayer = currentPlayer.GetComponent<Player>();
     }
 }
